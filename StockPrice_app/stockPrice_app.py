170dddbed16ad300d4dd73e7f6ec22130a6534af
Angel Santana <angelash303@gmail.com>
2022-03-22 18:41:23 -0600

    Code Clean up

--- StockPrice_app/stockPrice_app.py
+++ StockPrice_app/stockPrice_app.py
@@ -5,11 +5,12 @@
 import pandas as pd
 import numpy as np
 from datetime import date
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
 import streamlit as st
 
 # STOCK DATA FROM YAHOO FINANCE
 import yfinance as yf
-
 
 # =========================================================================================
 #                               CONFIGURE PAGE | PAGE DETAILS
@@ -21,7 +22,7 @@ st.set_page_config(page_title='Stock Price Prediction', # APP NAME FOR BROWSER
                     layout= 'wide' # PAGE LAYOUT
                     )
 
-st.title("Stock Price Prediction") # SET PAGE TITLE
+st.title("Stock Price Prediction 🤑💲") # SET PAGE TITLE
 # --- END CONFIG PAGE ---
 
 
@@ -32,15 +33,34 @@ st.markdown('---') # Add a page break
 st.header('How to use the web app?') 
 
 bullet_points = '''
-- Search for a stock using the search bar
-- Input the number of days you would like the price to be predicted
-- See a price trend graph
-- See the predicted stock price results
+- FIRST INPUT:
+    - Enter the ticker symbol for stock of your choice.
+    - You can use the table to the side to search through different stocks.
+- SECOND INPUT:
+    - Enter the amount of days out you would like to see the predicted stock price.
+    - ONLY positive numbers, please :)
+- As you update the stock you want to see, there will be a line graph to show you the price trend.
+
 
 '''
-st.markdown(bullet_points)
+with st.expander('🧑‍🏫 INSTRUCTIONS'):
+    st.markdown(bullet_points)
+
+not_a_recommendation = '''
+- This is not a tool to actually predict stock prices.
+- Do not consult this app for your PERSONAL choices on buying stock.
+- THIS APP IS JUST FOR FUN AND TO TEST MY PERSONAL KNOWLEDGE OF MACHINE LEARNING.
+
+'''
+
+with st.expander('⚠️ PLEASE READ'):
+    st.markdown(not_a_recommendation)
 
 st.markdown('---') # Add a page break
+
+# --- END DESCRIPTION ---
+# =========================================================================================
+
 
 # --- FUNCTION TO READ IN DATA ---
 def showStockNames():
@@ -55,3 +75,26 @@ def showStockNames():
 
 # --- END READ DATA FUNCTION ---
 
+
+# =========================================================================================
+#                                   INPUT LAYOUTS
+# =========================================================================================
+# PAGE LAYOUT
+user_column, forecast_column, stock_column = st.columns((1, 1, 2))
+
+# SYMBOL INPUT
+with user_column:
+    userInput = st.text_input('Enter a stock you would like to check?')
+    userInput = userInput.upper()
+
+# PREDICT NUMBER OF DAYS OUT
+with forecast_column:
+    forecastDays = st.number_input(label = 'Forecast Days...', step=1)
+    if forecastDays < 0:
+        st.write('ERROR: Number must be positive')
+
+# SHOW STOCKS FOR USERS TO LOOK THROUGH
+with stock_column:
+    st.write("Need help? Look for stock names HERE.")
+    st.dataframe(showStockNames())
+# =========================================================================================
